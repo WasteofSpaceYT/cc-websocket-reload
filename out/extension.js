8cda9af2c3f1983a66ac97590639b0ff91d0e27e
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-11-02 20:08:04 -0400

    Finished extension

--- out/extension.js
+++ out/extension.js
@@ -3,24 +3,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const ws_1 = require("ws");
-const https_1 = require("https");
-function encode(data) {
-    let string = "";
-    for (const [key, value] of Object.entries(data)) {
-        if (!value)
-            // eslint-disable-next-line curly
-            continue;
-        string += `&${encodeURIComponent(key)}=${encodeURIComponent(`${value}`)}`;
-    }
-    return string.substring(1);
-}
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 function activate(context) {
     let toggled = false;
     let menu = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-    let wsServer = new ws_1.WebSocketServer({ port: vscode.workspace.getConfiguration("cc-websocket-reload").get('port') });
-    wsServer.close();
+    let connectionCount = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+    connectionCount.text = '0';
+    connectionCount.show();
+    let wsServer;
+    try {
+        wsServer = new ws_1.WebSocketServer({ port: vscode.workspace.getConfiguration("cc-websocket-reload").get('port') });
+    }
+    catch (e) {
+        vscode.window.showErrorMessage("Failed to start websocket server" + " " + e);
+    }
+    try {
+        vscode.window.showInformationMessage(wsServer.path);
+    }
+    catch (e) {
+        vscode.window.showErrorMessage("Failed to get path" + " " + e);
+    }
+    if (wsServer) {
+        wsServer.on('connection', (ws) => {
+            vscode.window.showInformationMessage("Computer connected to websocket");
+            connectionCount.text = `Connections: ${wsServer.clients.size}`;
+            connectionCount.show();
+            ws.on("close", () => {
+                vscode.window.showInformationMessage("Computer disconnected from websocket");
+                connectionCount.text = `Connections: ${wsServer.clients.size}`;
+                connectionCount.show();
+            });
+        });
+    }
     if (toggled) {
         menu.text = "WebSocket Reload Enabled";
         menu.show();
@@ -32,58 +45,35 @@ function activate(context) {
     menu.command = 'cc-websocket-reload.toggle';
     context.subscriptions.push(menu);
     menu.show();
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "cc-websocket-reload" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('cc-websocket-reload.toggle', async () => {
-        if (!toggled) {
-            wsServer = new ws_1.WebSocketServer({ port: vscode.workspace.getConfiguration("cc-websocket-reload").get('port') });
+        if (!toggled && wsServer) {
             vscode.window.showInformationMessage("WebSocket server started on port " + vscode.workspace.getConfiguration("cc-websocket-reload").get('port'));
             if ((await vscode.workspace.findFiles("**/dist/**")).length > 0) {
                 let watcher = vscode.workspace.createFileSystemWatcher("**/dist/**");
                 watcher.onDidChange(async (e) => {
                     let file = await vscode.workspace.fs.readFile(vscode.Uri.file(e.path));
                     let url = e.path.split("/")[e.path.split("/").length - 1];
-                    try {
-                        (0, https_1.request)("https://pastebin.com/api/api_post.php", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/x-www-form-urlencoded"
-                            }
-                        }, (res) => {
-                            console.log("res recieved");
-                            res.setEncoding("utf8");
-                            res.on("data", (d) => {
-                                vscode.window.showInformationMessage(d);
-                            });
-                            res.on("error", (e) => {
-                                vscode.window.showErrorMessage(e);
-                            });
-                        }).end(JSON.stringify({
-                            api_dev_key: "3tEvadlShLxLBDoLJgyh8FL_IDPyD7Cm",
-                            api_option: "paste",
-                            api_paste_name: "Untitled",
-                            api_paste_code: 'print("cum")',
-                            api_paste_format: "lua",
-                            api_paste_private: 0,
-                            api_paste_expire_date: "N",
-                            api_user_key: "",
-                            api_folder_key: ""
-                        }));
-                    }
-                    catch (e) {
-                        vscode.window.showErrorMessage(e.toString());
-                    }
-                    vscode.window.showInformationMessage("res ended");
+                    wsServer.clients.forEach((client) => {
+                        client.send(JSON.stringify({ url: url, data: file.toString(), type: "change" }));
+                    });
+                    vscode.window.showInformationMessage("File changed: " + url);
                 });
                 watcher.onDidCreate(async (e) => {
-                    vscode.window.showInformationMessage(e.toString());
+                    let file = await vscode.workspace.fs.readFile(vscode.Uri.file(e.path));
+                    let url = e.path.split("/")[e.path.split("/").length - 1];
+                    wsServer.clients.forEach((client) => {
+                        client.send(JSON.stringify({ url: url, data: file.toString(), type: "create" }));
+                    });
+                    vscode.window.showInformationMessage("File created: " + url);
                 });
                 watcher.onDidDelete(async (e) => {
-                    vscode.window.showInformationMessage(e.path);
+                    let file = await vscode.workspace.fs.readFile(vscode.Uri.file(e.path));
+                    let url = e.path.split("/")[e.path.split("/").length - 1];
+                    wsServer.clients.forEach((client) => {
+                        client.send(JSON.stringify({ url: url, type: "delete" }));
+                    });
+                    vscode.window.showInformationMessage("File deleted");
                 });
                 vscode.window.showInformationMessage('WebSocket Reload Enabled');
             }
@@ -92,7 +82,6 @@ function activate(context) {
             }
         }
         else {
-            wsServer.close();
             vscode.window.showInformationMessage("WebSocket Reload Disabled");
         }
         toggled = !toggled;
